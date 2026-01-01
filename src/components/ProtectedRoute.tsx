febd02d2666a0833a971827b0cbeeaae96bdcad2
yasir5405 <yasirnaseem5415@gmail.com>
2026-01-02 00:43:38 +0530

    Fixed import of location from protected route

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,19 +1,28 @@
 import { useAuth } from "@/context/AuthContext";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({ allowedRoles }: { allowedRoles?: string[] }) => {
   const { user, loading } = useAuth();
+  const location = useLocation();
 
   if (loading) {
     return <div>Checking session...</div>;
   }
 
   if (!user) {
-    return <Navigate to={"/login"} state={{ from: location }} replace />;
+    return (
+      <Navigate to={"/login"} state={{ from: location.pathname }} replace />
+    );
   }
 
   if (allowedRoles && !allowedRoles.includes(user.role)) {
-    return <Navigate to={"/unauthorize"} />;
+    return (
+      <Navigate
+        to={"/unauthorize"}
+        state={{ from: location.pathname }}
+        replace
+      />
+    );
   }
 
   return <Outlet />;
